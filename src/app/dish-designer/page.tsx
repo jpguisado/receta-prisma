a4f24dbe7b76ab1e93af6a0f1a62c244f8f68ee4
José Pino <josepinoguisado@gmail.com>
2024-07-30 20:34:03 +0200

    Added recipe default value

--- src/app/dish-designer/page.tsx
+++ src/app/dish-designer/page.tsx
@@ -30,6 +30,7 @@ export default function DishDesigner() {
         resolver: zodResolver(createDishSchema),
         defaultValues: {
             name: '',
+            recipe: '',
         }
     })
 
