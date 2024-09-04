54a45b4ca23e57a04164ab0d3464a63400b0e463
José Pino <josepinoguisado@gmail.com>
2024-09-04 10:03:36 +0200

    Fixed RSC

--- src/app/shopping-day/page.tsx
+++ src/app/shopping-day/page.tsx
@@ -1,23 +1,5 @@
-import { Button } from "~/components/ui/button";
-import { Checkbox } from "~/components/ui/checkbox";
-import { Suspense, useActionState, useState } from "react";
-import { useFormStatus } from "react-dom";
-import { fetchDishList } from "~/server/data-layer";
-import { BrandNewDish } from "~/models/types/dish.td";
-
 export default async function Shopping() {
   
-  const listaPlatos: BrandNewDish[] = await fetchDishList();
-
-  // const listaPlatos: BrandNewDish[] = [{
-  //   name: 'TORTILLA A MANO',
-  //   ingredients: [{ ingredientId: 0, ingredient: { name: '' }, quantity: '0', quantityUnit: '1' }],
-  //   recipe: '',
-  //   id: 0,
-  // }]
-
-  // listaPlatos.map((plato) => plato.name)
-
   return (
     <>
       <p>Hello</p>
